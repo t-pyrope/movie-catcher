b2d8de032b118bfdbda05e95a6ecd42f452766b0
t-pyrope <kimmarina155@gmail.com>
2021-06-17 14:56:00 +0200

    added husky and lint-staged

--- src/components/Header/SearchComponent.js
+++ src/components/Header/SearchComponent.js
@@ -13,6 +13,8 @@ const SearchComponent = () => {
   const [textInput, setTextInput] = useState('');
   const [liveSearchMovies, setLiveSearchMovies] = useState([]);
 
+  console.log('hello');
+
   useEffect(() => {
     liveSearch.current.classList.add('searchForm__liveSearch_hidden');
     setLiveSearchMovies([]);
