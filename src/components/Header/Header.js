b9e943f7a036a415a4fa96df41971affbdf28d28
badylbek <Begaiym.Adylbek.Kyzy@teskalabs.com>
2021-09-29 18:52:04 +0200

    Fix not closing dropdown

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,54 +18,36 @@ const Header = () => {
 
   const { genres } = useSelector((state) => state.genres);
 
-  const openGenreListHandler = (e) => {
+  const openListHandler = (e) => {
     const parent = e.target.parentElement;
     parent.children[1].style.opacity = 1;
     parent.children[1].style.pointerEvents = 'all';
     return null;
   };
 
-  const closeGenreListHandler = (e) => {
-    if (e.target.classList.contains('genre-list')) {
-      e.target.style.opacity = 0;
-      e.target.style.pointerEvents = 'none';
-    }
-    if (e.target.classList.contains('genres')) {
-      e.target.children[0].style.opacity = 0;
-      e.target.children[0].pointerEvents = 'none';
+  const closeListHandler = (e) => {
+    const { classList } = e.target;
+    if (classList.contains('list')) {
+      const { style } = e.target;
+      style.opacity = 0;
+      style.pointerEvents = 'none';
+    } else if (classList.contains('list-item')) {
+      const { style } = e.target.parentElement;
+      style.opacity = 0;
+      style.pointerEvents = 'none';
     }
   };
 
-  const genreSelectHandler = (e) => {
+  const selectHandler = (e) => {
     e.stopPropagation();
-    const genreId = e.target.id;
-    dispatch(loadGenreMovies(genreId, '1', 'popularity.desc'));
-    history.push(`/genres/${genreId}`);
-    const div = e.target.parentElement.parentElement;
-    div.style.opacity = 0;
-    div.style.pointerEvents = 'none';
-  };
-  const openPopularInHandler = (e) => {
-    const parent = e.target.parentElement;
-    parent.children[1].style.opacity = 1;
-    parent.children[1].style.pointerEvents = 'all';
-  };
-
-  const closePopularInHandler = (e) => {
-    if (e.target.classList.contains('popular-list')) {
-      e.target.style.opacity = 0;
-      e.target.style.pointerEvents = 'none';
+    const selectId = e.target.id;
+    const selectName = e.target.name;
+    if (selectName === 'genre') {
+      dispatch(loadGenreMovies(selectId, '1', 'popularity.desc'));
+      history.push(`/genres/${selectId}`);
     }
-    if (e.target.classList.contains('popular')) {
-      e.target.children[0].style.opacity = 0;
-      e.target.children[0].pointerEvents = 'none';
-    }
-  };
+    if (selectName === 'year') history.push(`/year/${selectId}`);
 
-  const yearSelectHandler = (e) => {
-    e.stopPropagation();
-    const genreId = e.target.id;
-    history.push(`/year/${genreId}`);
     const div = e.target.parentElement.parentElement;
     div.style.opacity = 0;
     div.style.pointerEvents = 'none';
@@ -86,16 +68,24 @@ const Header = () => {
             <button
               className="mainNav__button mainNav__button_navItem"
               type="button"
-              onClick={openGenreListHandler}
-              onKeyDown={openGenreListHandler}
+              onClick={openListHandler}
+              onKeyDown={openListHandler}
             >
               Genres
             </button>
             {genres.length && (
-              <div className="genre-list dropdown" onMouseLeave={closeGenreListHandler}>
+              <div className="list dropdown" onMouseLeave={closeListHandler}>
                 {genres.map((genre) => (
-                  <p key={genre.id}>
-                    <button className="mainNav__button mainNav__button_navItem" type="button" id={genre.id} onClick={genreSelectHandler}>{genre.name}</button>
+                  <p key={genre.id} className="list-item">
+                    <button
+                      name="genre"
+                      className="mainNav__button mainNav__button_navItem"
+                      type="button"
+                      id={genre.id}
+                      onClick={selectHandler}
+                    >
+                      {genre.name}
+                    </button>
                   </p>
                 ))}
               </div>
@@ -104,14 +94,24 @@ const Header = () => {
           <li className="mainNav__link mainNav__link_relative">
             <button
               type="button"
-              onClick={openPopularInHandler}
+              onClick={openListHandler}
               className="mainNav__button"
             >
               Popular in...
             </button>
-            <div className="popular-list dropdown" onMouseLeave={closePopularInHandler}>
+            <div className="list dropdown" onMouseLeave={closeListHandler}>
               {years.map((year) => (
-                <p key={year}><button type="button" onClick={yearSelectHandler} id={year} className="mainNav__button">{year}</button></p>
+                <p key={year} className="list-item">
+                  <button
+                    type="button"
+                    name="year"
+                    onClick={selectHandler}
+                    id={year}
+                    className="mainNav__button"
+                  >
+                    {year}
+                  </button>
+                </p>
               ))}
             </div>
           </li>
